8031c10b6ab0ff71694ccdb2e9f034ba1b6b75ca
Poli Júnior <polijr@users.noreply.github.com>
2021-02-22 18:29:22 -0300

    Initial commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+import 'express-async-errors';
+
+import '@shared/infra/typeorm';
+
+import AppError from '@shared/errors/AppError';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/teste', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server started on port ${process.env.PORT || 3333}`);
+});
